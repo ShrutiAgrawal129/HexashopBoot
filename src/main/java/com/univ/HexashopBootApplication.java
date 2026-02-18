2c2c32c7441b6d7af7aca0f1a13894b3bfffa457
Shruti <agrawalshruti129@gmail.com>
2026-02-19 02:44:45 +0530

    Converted to WAR for JSP compatibility

--- src/main/java/com/univ/HexashopBootApplication.java
+++ src/main/java/com/univ/HexashopBootApplication.java
@@ -2,10 +2,18 @@ package com.univ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HexashopBootApplication {
+public class HexashopBootApplication extends SpringBootServletInitializer{
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(HexashopBootApplication.class);
+	    }
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HexashopBootApplication.class, args);
 	}
